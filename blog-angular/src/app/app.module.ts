f4deac3b6f7e6b88cf23b6c2b86902d92cd23179
deydrums <dagarcia100@gmail.com>
2021-06-09 16:17:57 -0600

    Componente de registro

--- blog-angular/src/app/app.module.ts
+++ blog-angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
