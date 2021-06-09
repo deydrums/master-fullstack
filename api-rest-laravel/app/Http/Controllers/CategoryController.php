63f07c960778f8c592d08d91eab152d1a10dd553
deydrums <dagarcia100@gmail.com>
2021-06-09 10:08:04 -0600

    Guardar categoria

--- api-rest-laravel/app/Http/Controllers/CategoryController.php
+++ api-rest-laravel/app/Http/Controllers/CategoryController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+
 class CategoryController extends Controller
 {
+
+    public function __construct(){
+        $this->middleware('api.auth',['except' =>['index', 'show']]);
+    }
     public function index(){
         $categories = Category::all();
         return response()->json([
@@ -31,8 +37,43 @@ class CategoryController extends Controller
             ];
         }
         return response()->json($data,$data['code']);
+    }
 
+    public function store(request $request){
+        //Recoger los datos por post
+        $json = $request->input('json',null);
+        $params_array = json_decode($json,true);
 
-
+        if(!empty($params_array)){
+            //Validar los datos
+            $validate = Validator::make($params_array,[
+                'name' => 'required'
+            ]);
+            //Guardar la categoria
+            if($validate->fails()){
+                $data = [
+                    'code' => 400,
+                    'status' =>'error',
+                    'message' => 'No se ha guardado la categoria'
+                ];
+            }else{
+                $category = new Category();
+                $category->name = $params_array['name'];
+                $category->save();
+                $data = [
+                    'code' => 200,
+                    'status' =>'success',
+                    'category' => $category
+                ];
+            }
+        }else{
+            $data = [
+                'code' => 400,
+                'status' =>'error',
+                'message' => 'No se ha enviado ninguna categoria'
+            ];       
+        }
+        //Devolver resultado
+        return response()->json($data,$data['code']);
     }
 }
