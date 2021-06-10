4374cdc832b81f17e450f714eee7583128b245be
deydrums <dagarcia100@gmail.com>
2021-06-09 22:38:26 -0600

    Mostrar datos del usuario identificado

--- blog-angular/src/app/app.component.ts
+++ blog-angular/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { UserService } from './services/user.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
 export class AppComponent {
-  title = 'blog-angular';
+  public title = 'blog-angular';
+  public identity:any;
+  public token:any;
+
+  constructor(
+    public _userService: UserService
+  ){
+    this.identity = this._userService.getIdentity();
+  }
 }
--- blog-angular/src/app/services/user.service.ts
+++ blog-angular/src/app/services/user.service.ts
@@ -9,6 +9,8 @@ import { global } from './global';
 export class UserService {
 
     public url: string;
+    public identity!: any;
+    public token!: any;
 
     constructor(public _http: HttpClient){
         this.url = global.url;
@@ -34,6 +36,26 @@ export class UserService {
         let params = 'json='+json;
         let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
         return this._http.post(this.url+'login',params,{headers:headers});
+    }
+
+    getIdentity(){
+        let identity = JSON.parse(localStorage.getItem('identity') || '{}');
+        if(identity && identity != "undefined"){
+            this.identity = identity;
+        }else{
+            this.identity = null;
+        }
+        return this.identity;
+    }
+
+    getToken(){
+        let token = localStorage.getItem('token');
+        if(token && token != "undefined"){
+            this.token = token;
+        }else{
+            this.token = null;
+        }
+        return this.token;
     }
 
 }
