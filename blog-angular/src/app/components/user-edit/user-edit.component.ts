5ebed0f93c2cf347cfa231c0da28db75f2413c30
deydrums <dagarcia100@gmail.com>
2021-06-10 15:36:14 -0600

    Rellenar formulario de ajustes

--- blog-angular/src/app/components/user-edit/user-edit.component.ts
+++ blog-angular/src/app/components/user-edit/user-edit.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
-  styleUrls: ['./user-edit.component.css']
+  styleUrls: ['./user-edit.component.css'],
+  providers: [UserService]
 })
 export class UserEditComponent implements OnInit {
   public page_title: string;
   public user: User;
-  constructor() { 
+  public identity;
+  public token;
+
+  constructor(private _userService: UserService) { 
     this.page_title = 'Ajustes';
     this.user = new User(1,'','','ROLE_USER','','','','');
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+    this.user = this.identity;
   }
     
   ngOnInit(): void {
