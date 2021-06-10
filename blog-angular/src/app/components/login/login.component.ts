28b72f40157fd68a4b1393b432d47499872a8145
deydrums <dagarcia100@gmail.com>
2021-06-09 23:02:43 -0600

    Cerrar sesion

--- blog-angular/src/app/components/login/login.component.ts
+++ blog-angular/src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 import { faThList } from '@fortawesome/free-solid-svg-icons';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
@@ -19,7 +20,9 @@ export class LoginComponent implements OnInit {
   public identity:any;
 
   constructor(
-    private _userService: UserService
+    private _userService: UserService,
+    private _router: Router,
+    private _route: ActivatedRoute
   ) { 
     this.page_title = 'Identificate';
     this.user = new User(1,'','','ROLE_USER','','','','');
@@ -27,6 +30,7 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.logout();
   }
 
   onSubmit(form:any){
@@ -45,6 +49,8 @@ export class LoginComponent implements OnInit {
                 //Persistir datos usuario identificado
                 localStorage.setItem('token', this.token);
                 localStorage.setItem('identity', JSON.stringify(this.identity));
+                //Redireccion a inicio
+                this._router.navigate(['inicio']);
             },
             error =>{
               this.status = 'error';
@@ -61,5 +67,20 @@ export class LoginComponent implements OnInit {
       }
     );
     
+  }
+
+  logout() {
+    this._route.params.subscribe(params =>{
+      let logout = +params['sure'];
+      if(logout == 1){
+        localStorage.removeItem('identity');
+        localStorage.removeItem('token');
+        this.identity = null;
+        this.token = null;
+
+        //Redireccion a inicio
+        this._router.navigate(['inicio']);
+      }
+    });
   }
 }
