cfe58b24133eb9305e82c370dd3290bf45f40899
deydrums <dagarcia100@gmail.com>
2021-06-09 22:12:28 -0600

    Identificar al usuario

--- blog-angular/src/app/components/login/login.component.ts
+++ blog-angular/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { faThList } from '@fortawesome/free-solid-svg-icons';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
 
@@ -12,9 +14,12 @@ export class LoginComponent implements OnInit {
 
   public page_title: string;
   public user:User;
+  public status!:string;
+  public token:any;
+  public identity:any;
 
   constructor(
-    private userService: UserService
+    private _userService: UserService
   ) { 
     this.page_title = 'Identificate';
     this.user = new User(1,'','','ROLE_USER','','','','');
@@ -25,6 +30,32 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(form:any){
-    console.log(this.user);
+    this._userService.signup(this.user).subscribe(
+      response =>{
+        //TOKEN
+        if(response.status != 'error'){
+          this.status = 'success';
+          this.token = response;
+
+          //Objeto usuario identificado
+          this._userService.signup(this.user, true).subscribe(
+            response =>{
+                this.identity = response;
+            },
+            error =>{
+              this.status = 'error';
+              console.log(<any>error);
+            }
+          );
+        }else{
+          this.status = 'error';
+        }
+      },
+      error =>{
+        this.status = 'error';
+        console.log(<any>error);
+      }
+    );
+    
   }
 }
--- blog-angular/src/app/services/user.service.ts
+++ blog-angular/src/app/services/user.service.ts
@@ -26,4 +26,14 @@ export class UserService {
         return this._http.post(this.url+'register',params,{headers:headers});
     }
 
+    signup(user:any, gettoken:any = null):Observable<any>{
+        if(gettoken != null){
+            user.gettoken = 'true';
+        }
+        let json = JSON.stringify(user);
+        let params = 'json='+json;
+        let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+        return this._http.post(this.url+'login',params,{headers:headers});
+    }
+
 }
