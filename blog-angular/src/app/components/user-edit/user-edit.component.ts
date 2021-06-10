e69def8a0f3c91ba77ed31246e6cc95fb8394cf5
deydrums <dagarcia100@gmail.com>
2021-06-10 17:00:13 -0600

    Actualizar los datos del usuario

--- blog-angular/src/app/components/user-edit/user-edit.component.ts
+++ blog-angular/src/app/components/user-edit/user-edit.component.ts
@@ -13,19 +13,63 @@ export class UserEditComponent implements OnInit {
   public user: User;
   public identity;
   public token;
+  public status!: any;
 
   constructor(private _userService: UserService) { 
     this.page_title = 'Ajustes';
     this.user = new User(1,'','','ROLE_USER','','','','');
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
-    this.user = this.identity;
+    //Rellenar
+    this.user = new User(
+      this.identity.sub,
+      this.identity.name,
+      this.identity.surname,
+      this.identity.role,
+      this.identity.email,'',
+      this.identity.description,  
+      this.identity.image
+    );
   }
     
   ngOnInit(): void {
   }
 
   onSubmit(form:any){
-  
+    this._userService.update(this.token, this.user).subscribe(
+      response =>{
+        if(response){
+          console.log(response.user);
+          this.status = 'success';
+          //Actualizar usuario en sesion
+          if(response.changes.name){
+            this.user.name = response.changes.name;
+          }
+          if(response.changes.surname){
+            this.user.surname = response.changes.surname;
+          }
+          if(response.changes.email){
+            this.user.email = response.changes.email;
+          }
+          if(response.changes.description){
+            this.user.description = response.changes.description;
+          }
+          if(response.changes.image){
+            this.user.image = response.changes.image;
+          }
+          //Actualizar usuario en el local storage
+          this.identity = this.user;
+          localStorage.setItem('identity',JSON.stringify(this.identity));
+        }else{
+          this.status = 'error';
+        }
+        
+      },
+      error =>{
+        this.status = 'error';
+        console.log(<any>error);
+      }
+    );
+
   }
 }
