21a4e37ba022e581d2a6323228aa4e685554fa83
deydrums <dagarcia100@gmail.com>
2021-06-14 15:08:44 -0600

    Mostrar los botones a los usuarios identificados

--- blog-angular/src/app/components/home/home.component.ts
+++ blog-angular/src/app/components/home/home.component.ts
@@ -2,27 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { PostService } from '../../services/post.service';
 import { Post } from '../../models/post';
 import { global } from '../../services/global';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  providers: [PostService]
+  providers: [PostService, UserService]
 })
 export class HomeComponent implements OnInit {
   page_title: string;
   public posts!: Array<Post>;
   public url;
-
+  public identity;
+  public token;
+  
   constructor(
-    private _postService: PostService
+    private _postService: PostService,
+    private _userService: UserService
   ) {
     this.page_title = "Pagina de inicio";
     this.url = global.url;
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
    }
 
   ngOnInit(): void {
     this.getPosts();
+    console.log(this.identity);
   }
 
 
