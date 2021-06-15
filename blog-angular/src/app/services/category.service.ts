f39ed4f6d4ebb08c23d6bf2d7f4038f6e7ef2368
deydrums <dagarcia100@gmail.com>
2021-06-15 17:24:20 -0600

    Nuevos metodos para categoria

--- blog-angular/src/app/services/category.service.ts
+++ blog-angular/src/app/services/category.service.ts
@@ -32,4 +32,15 @@ export class CategoryService {
         let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
         return this._http.get(this.url+'category',{headers:headers});
     }
+
+    getCategory(id:any):Observable<any>{
+        let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+        return this._http.get(this.url+'category/'+id,{headers:headers});
+    }
+
+    getPosts(id:any):Observable<any>{
+        let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+        return this._http.get(this.url+'post/category/'+id,{headers:headers});
+    }
+
 }
