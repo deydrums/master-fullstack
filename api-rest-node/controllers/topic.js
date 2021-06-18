dc552efaa1800beb5b84d65286102ea004413e10
deydrums <dagarcia100@gmail.com>
2021-06-18 16:21:35 -0600

    Metodo para guardar nuevos temas

--- api-rest-node/controllers/topic.js
+++ api-rest-node/controllers/topic.js
@@ -4,6 +4,17 @@ var controller ={
 
     test: function(req, res){
         return res.status(200).send({message: "Hola"});
+    },
+
+    save: function(req, res){
+        //Recoger los parametros de la peticion
+        //Validar los datos
+        //Crear objeto a guardar
+        //Asignar valores
+        //Guardar el topic
+
+        //Devolver una respuesta
+        return res.status(200).send({message: "Guardar topic"});
     }
 }
 
