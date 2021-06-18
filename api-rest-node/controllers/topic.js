02fb1afae165694671d02c71e29c078662a5434c
deydrums <dagarcia100@gmail.com>
2021-06-18 17:33:58 -0600

    Configuracion de la paginacion

--- api-rest-node/controllers/topic.js
+++ api-rest-node/controllers/topic.js
@@ -37,6 +37,7 @@ var controller ={
         topic.content = params.content;
         topic.code = params.code;
         topic.lang = params.lang;
+        topic.user = req.user.sub;
         //Guardar el topic
         topic.save((err, topicStored)=>{
             if(err || !topicStored){
@@ -49,11 +50,30 @@ var controller ={
     },
 
     getTopics: function(req, res){
+
+        //Cargar la libreria de paginacion en la clase ------> Modelo
+        //Recoger la pagina actual
+        var page = req.params.page;
+        if(req.params.page == null || req.params.page ==undefined || !req.params.page || req.params.page == 0 || req.params.page == '0'){
+            var page = 1;
+        }else{
+            var page = parseInt(req.params.page);
+        }
+        //Indicar las opciones de paginacion
+        var options = {
+            sort: {date: -1},
+            populate: 'user',
+            limit: 4,
+            page: page
+
+        };
+        //Find paginador
+        //Devolver resultado (topics, total, total de paginas)
         Topic.find().exec((err,topics)=>{
             if(err || !topics){
                 return res.status(404).send({status: 'error', message: 'No se han encontrado temas.'});
             }
-            return res.status(200).send({status: 'success', topics});
+            return res.status(200).send({status: 'success', topics, page,options});
         });
     }
 }
--- api-rest-node/models/topic.js
+++ api-rest-node/models/topic.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var mongoose = require('mongoose');
+var mongoosePaginate = require('mongoose-paginate-v2');
 var Schema = mongoose.Schema;
 
 //Modelo de COMMENT
@@ -22,5 +23,6 @@ var TopicSchema = Schema({
     user: {type: Schema.ObjectId, ref: 'User'},
     comments: [CommentSchema]
 });
-
+//Cargar paginacion 
+TopicSchema.plugin(mongoosePaginate);
 module.exports = mongoose.model('Topic', TopicSchema);
