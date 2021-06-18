f155a4b8d13c5b2b2736c71674db12a0e700c7e9
deydrums <dagarcia100@gmail.com>
2021-06-18 16:47:28 -0600

    Metodo para sacar los topics

--- api-rest-node/routes/topic.js
+++ api-rest-node/routes/topic.js
@@ -8,6 +8,6 @@ var md_auth = require('../middlewares/authenticated');
 
 router.get('/test',TopicController.test);
 router.post('/topic',md_auth.authenticated,TopicController.save);
-router.get('/topics',TopicController.getTopics);
+router.get('/topics/:page?',TopicController.getTopics);
 
 module.exports = router;
