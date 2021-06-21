a0d36a2748223afce1bc107c9c4e260e71ffcb11
deydrums <dagarcia100@gmail.com>
2021-06-21 12:16:04 -0600

    Cerrar sesion

--- foro-angular/src/app/app.component.ts
+++ foro-angular/src/app/app.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import { UserService } from './services/user.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   providers: [UserService]
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, DoCheck{
   title = 'foro-angular';
   public identity;
   public token;
   constructor(
     private _userService: UserService,
+    private _router: Router,
+    private _route: ActivatedRoute
 
   ){
     this.identity = this._userService.getIdentity();
@@ -19,7 +22,20 @@ export class AppComponent implements OnInit{
   }
 
   ngOnInit() {
-    console.log(this.identity);
-    console.log(this.token);
+    // console.log(this.identity);
+    // console.log(this.token);
   }
+
+  ngDoCheck() {
+    this.identity = this._userService.getIdentity();
+  }
+
+  logout() {
+    localStorage.clear();
+    this.token = null;
+    this.identity = null;
+    this._router.navigate(['/inicio']);
+  }
+
 }
+
