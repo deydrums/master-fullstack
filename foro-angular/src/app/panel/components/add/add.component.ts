e4dfad7f3e3446d8b7e93d4e1946c5845a69d752
deydrums <dagarcia100@gmail.com>
2021-06-21 17:27:55 -0600

    Preparar el componente de crear tema

--- foro-angular/src/app/panel/components/add/add.component.ts
+++ foro-angular/src/app/panel/components/add/add.component.ts
@@ -1,13 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Topic } from '../../../models/topic';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.css']
+  styleUrls: ['./add.component.css'],
+  providers: [UserService]
 })
 export class AddComponent implements OnInit {
-
-  constructor() { }
+  public page_title: string;
+  public topic: Topic;
+  public identity: any;
+  public token: string;
+  public status: any;
+  constructor(
+    private _userService: UserService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ) { 
+    this.page_title = "Crear nuevo tema";
+    this.topic = new Topic('','','','','','',this.identity._id,null);
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+  }
 
   ngOnInit(): void {
   }
