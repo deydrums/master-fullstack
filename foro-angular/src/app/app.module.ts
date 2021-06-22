a5d18fa9d3f89cfc6c6839780f268ebf2b7bee6d
deydrums <dagarcia100@gmail.com>
2021-06-22 16:30:13 -0600

    Crear componente para los perfiles de usuario

--- foro-angular/src/app/app.module.ts
+++ foro-angular/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { UserService } from './services/user.service';
 import { UserGuard } from './services/user.guard';
 import { NoIdentityGuard } from './services/no.identity.guard';
 import { UsersComponent } from './components/users/users.component';
+import { ProfileComponent } from './components/profile/profile.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -37,7 +38,8 @@ import { UsersComponent } from './components/users/users.component';
     UserEditComponent,
     TopicsComponent,
     TopicDetailComponent,
-    UsersComponent
+    UsersComponent,
+    ProfileComponent
     // MainComponent,
     // AddComponent,
     // EditComponent,
