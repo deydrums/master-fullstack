d76b47f2f74c64fdf1765396e9df4c45513286fb
deydrums <dagarcia100@gmail.com>
2021-06-22 09:57:40 -0600

    Preparar el componente de temas

--- foro-angular/src/app/components/topics/topics.component.ts
+++ foro-angular/src/app/components/topics/topics.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Topic } from '../../models/topic';
+import { TopicService } from 'src/app/services/topic.service';
+
 
 @Component({
   selector: 'app-topics',
   templateUrl: './topics.component.html',
-  styleUrls: ['./topics.component.css']
+  styleUrls: ['./topics.component.css'],
+  providers: [TopicService]
 })
 export class TopicsComponent implements OnInit {
-
-  constructor() { }
+  public page_title: string;
+  public topics: Topic[] = [];
+  public totalPages!:any;
+  public page!:any;
+  public next_page!:any;
+  public prev_page!:any;
+  constructor(
+    private _topicService: TopicService,
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) { 
+    this.page_title = "Temas";
+  }
 
   ngOnInit(): void {
   }
--- foro-angular/src/app/services/topic.service.ts
+++ foro-angular/src/app/services/topic.service.ts
@@ -40,4 +40,8 @@ export class TopicService {
         let headers = new HttpHeaders().set('Content-Type', 'application/json').set("Authorization",token);
         return this._http.delete(this.url+'topic/'+id,{headers:headers});
     }
+
+    getTopics(page = 1):Observable<any>{
+        return this._http.get(this.url + 'topic/'+page);
+    }
 }
