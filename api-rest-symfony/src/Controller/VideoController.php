c902fea6638bcd7652315cccb9a246d41bca4720
deydrums <dagarcia100@gmail.com>
2021-06-23 18:38:08 -0600

    Accion y ruta para guardar nuevo video

--- api-rest-symfony/src/Controller/VideoController.php
+++ api-rest-symfony/src/Controller/VideoController.php
@@ -4,14 +4,39 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Constraints\Email;
+use App\Services\JwtAuth;
+use App\Entity\User;
+use App\Entity\Video;
 
 class VideoController extends AbstractController
 {
+    private function resjson($data){
+        //Serializar datos con servicio serializer
+        $json = $this->get('serializer')->serialize($data, 'json');
+        //Respose HttpFoundation
+        $response = new Response();
+        //Asignar contenido a la respuesta
+        $response->setContent($json);
+        //Indicar formato de respuesta
+        $response->headers->set('Content-Type', 'application/json');
+        //Devolver respuesta
+        return $response;
+    }
+
     public function index(): Response
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/VideoController.php',
         ]);
+    }
+
+    public function create(Request $request, JwtAuth $jwt_auth){
+        $data =['code' => 200,'status' => 'success','message' => 'Video creado exitosamente.'];
+        return $this->resjson($data);
     }
 }
